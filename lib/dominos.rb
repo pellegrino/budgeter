8ae9fa55231cfa17a3201c89e3ad2c90a3388529
Vitor Pellegrino <vitorp@gmail.com>
2011-04-19 14:38:58 -0300

    Removing unused file

--- lib/dominos.rb
+++
@@ -1,7 +0,0 @@
-module Dom
-  # Check https://github.com/ngauthier/domino for documentation 
-  class Transaction < Domino
-    selector '#transactions li'
-    attribute :description, 'p'
-  end
-end
