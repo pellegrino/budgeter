9c8d944d61ba48fec89f7e6ae94eacabe2528c2e
Vitor Pellegrino <vitorp@gmail.com>
2012-02-26 23:06:32 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Budgeter::Application.config.secret_token = 'a0c60eec7fb2ddb3bac6b09fd0b5a3b56589c15a90a64edaff47bd4ce947858c2dd079053b4e2ca3593bdb6bb59d79af27639044f892a7daffe1e54ad5a7a2e5'
